64c95333bc163a8aa2f7cfee283d7d4c07fee3ed
cri cri <depa@live.com.ar>
2020-05-25 21:56:52 -0300

    init comit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const botName = 'CriCriBot';
+
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
+
+        socket.emit('message', formatMessage(botName, `Bienvenid@ al chat!`));
+
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} entro a la sala`));
+
+        io.to(user.room).emit('roomUsers',{
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+
+    })
+
+    socket.on('chatMessage', (msg) => {
+
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    })
+    socket.on('disconnect', () => {
+        const  user = userLeave(socket.id);
+
+        if(user){
+            io.to(user.room).emit(
+                'message',
+                formatMessage(botName, `${user.username} se fue de la sala u.u`));
+                
+            io.to(user.room).emit('roomUsers',{
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })    
+        } 
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
